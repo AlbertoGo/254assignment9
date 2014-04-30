8286d9457eaa9a02d93faadb20d5ab5803973c5f
Alberto Gomez <otrebla117@csu.fullerton.edu>
2014-04-30 11:02:38 -0700

    add header

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
+#include "name.h"
+
 using namespace std;
 
 
 int main()
 {
 	cout << "Hello Git World!!!" << endl;
+	printName();
 
 	return 0;
 }
---
+++ name.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
+void printName(){
+	cout << "Hello user";
+}
